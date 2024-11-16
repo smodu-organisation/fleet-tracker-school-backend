0ae318348114de21127b545c4dfbd202bf567eb9
med20201 <abouammar.med12@gmail.com>
2024-11-16 03:17:10 +0100

    delete all  a test routes

--- routes/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const routeController = require('../controllers/routeController');
-const router = express.Router();
-
-router.post('/add', routeController.addRoute);
-router.get('/', routeController.getAllRoutes);
-
-module.exports = router;
