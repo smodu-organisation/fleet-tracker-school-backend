f197c7bccc3d357e0e7c0e252706d1235042f86a
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2024-11-21 22:52:41 +0100

    feat: the users can can a session in a single device only

--- middlewares/sessionMiddleware.js
+++ middlewares/sessionMiddleware.js
@@ -2,9 +2,11 @@ const Session = require('../models/Session');
 
 exports.ensureSingleSession = async (req, res, next) => {
   const userId = req.user.id;
+  const deviceId = req.deviceId; 
+
   const existingSession = await Session.findOne({ userId });
 
-  if (existingSession && existingSession.deviceId !== req.deviceId) {
+  if (existingSession && existingSession.deviceId !== deviceId) {
     return res.status(403).json({ error: 'Session already active on another device.' });
   }
 
