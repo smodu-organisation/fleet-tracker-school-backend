5fe1af79b2d428dd45374bdd44aa15b757122d57
aminemaliki7 <aminemaliki79@example.com>
2024-11-24 17:23:30 +0100

    Added notification management and retrieval functionality

--- routes/notification.js
+++ routes/notification.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const notificationController = require('../controllers/notificationController');
 const { verifyToken, verifyRole } = require('../middlewares/authMiddleware'); 
 
-router.post('/send', notificationController.sendNotification);
+router.post('/send',verifyToken, verifyRole('manager'), notificationController.sendNotification);
 router.post('/schedule', verifyToken, verifyRole('manager'), notificationController.scheduleNotification);
 
-router.get('/:userId', notificationController.getNotifications);
+router.get('/:userId',verifyToken, verifyRole('manager'), notificationController.getNotifications);
 router.put('/:notificationId/mark-as-read', verifyToken, notificationController.markAsRead);
-router.get('/sent/:senderId', notificationController.getNotificationsSentByUser);
+router.get('/sent/:senderId',verifyToken, verifyRole('manager'), notificationController.getNotificationsSentByUser);
 
 module.exports = router;
